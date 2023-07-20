8612ff63a8fe633d7ebfc7bb3abd7f3957f7dde5
Sam Wells <sfwells0518@gmail.com>
2023-07-20 16:39:07 -0500

    add headers const to index.jsx file and pass headers through in each section of code making an api call

--- index.jsx
+++ index.jsx
@@ -20,14 +20,14 @@ document.getElementById("send-btn").addEventListener("click", () => {
   }
 });
 
+const headers = {
+  "User-Agent": "SW Movie Gen App"
+};
+
 async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
+    headers: headers,
     model: "text-davinci-003",
-    /*
-    1. Refactor this prompt to use examples of an outline and an 
-    enthusiastic response. Be sure to keep the length of your 
-    examples reasonably short, say 20 words or so.
-    */
     prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need a few moments to think about it.
     ###
     outline: Two dogs fall in love and move to Hawaii to learn to surf.
@@ -49,12 +49,8 @@ async function fetchBotReply(outline) {
 
 async function fetchSynopsis(outline) {
   const response = await openai.createCompletion({
+    headers: headers,
     model: "text-davinci-003",
-    /*
-    1. Refactor the prompt to use one or more examples. 
-    2. Remember to separate the instruction from the 
-     example with ### or '''.
-    */
     prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors' names in brackets after each character. Choose actors that would particularly suit the role.
     ###
     outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
@@ -64,7 +60,6 @@ async function fetchSynopsis(outline) {
     synopsis: 
     `,
     max_tokens: 700,
-    // potential new prompt: `
   });
   const synopsis = response.data.choices[0].text.trim();
   document.getElementById("output-text").innerText = synopsis;
@@ -74,6 +69,7 @@ async function fetchSynopsis(outline) {
 
 async function fetchTitle(synopsis) {
   const response = await openai.createCompletion({
+    headers: headers,
     model: "text-davinci-003",
     prompt: `Generate a catchy movie title for this synopsis: 
     ${synopsis}`,
@@ -87,8 +83,8 @@ async function fetchTitle(synopsis) {
 
 async function fetchStars(synopsis) {
   const response = await openai.createCompletion({
+    headers: headers,
     model: "text-davinci-003",
-    /* extract the names in brackets from our synopsis. */
     prompt: `Extract the names in brackets from the synopsis.
     ###
     synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
@@ -104,6 +100,7 @@ async function fetchStars(synopsis) {
 
 async function fetchImagePrompt(title, synopsis) {
   const response = await openai.createCompletion({
+    headers: headers,
     model: "text-davinci-003",
     prompt: `Give me a short description of an image which could be used to advertise a movie based on a title and synopsis. The description should be rich in visual detail, but contain no names.
     ###
@@ -127,6 +124,7 @@ async function fetchImagePrompt(title, synopsis) {
 
 async function fetchImageUrl(imagePrompt) {
   const response = await openai.createImage({
+    headers: headers,
     prompt: `${imagePrompt}. There should be no text in this image.`,
     n: 1,
     size: "256x256",
